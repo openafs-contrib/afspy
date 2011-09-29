bb859adb83b2f44e93bc0722d44fdac3ca549a1e
hanke <hanke@b2240afc-07ac-11e0-b136-6b3c288fb16b>
2011-09-29 13:37:34 +0000

    Add directory for unittests..
    git-svn-id: http://pfanne.rzg.mpg.de/svn/ro/hanke/openafs-plumbing/afspy@75 b2240afc-07ac-11e0-b136-6b3c288fb16b

--- tests/VolServiceTests.py
+++ tests/VolServiceTests.py
@@ -1,10 +1,48 @@
+#!/usr/bin/env python
+
 import unittest
+import sys
+from ConfigParser import ConfigParser
+
+sys.path.append("..")
+
+from afs.model.AfsConfig import AfsConfig
+from afs.service.VolService import VolService
+from afs.model.Token import Token
+from TestConfig import TestConfig
+
 
 class TestVolServiceMethods(unittest.TestCase):
     """
     Tests VolService Methods
     """
+        
+    def setUp(self):
+        """
+        setup token and VolService
+        """
+        self.TestCfg=ConfigParser()
+        self.TestCfg.read("tests.cfg")
+        self.Cell=self.TestCfg.get("general", "Cell")
+        self.User=self.TestCfg.get("general", "User")
+        self.Pass=self.TestCfg.get("general", "Pass")
+        token = Token(self.User, self.Pass, self.Cell)
+        self.volMng = VolService(token)
+        self.VolID=self.TestCfg.get("VolService", "VolID")
+        self.VolName=self.TestCfg.get("VolService", "VolName")
+        return
+    
+    def test_getVolbyName(self) :
+        vol = self.volMng.getVolByName(self.VolName)
+        self.assertEqual(vol.vid, self.VolID)
+        return
+        
+    def test_getVolbyID(self) :
+        vol = self.volMng.getVolByID(self.VolID)
+        self.assertEqual(vol.name, self.VolName)
+        return
     
 
 if __name__ == '__main__' :
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestVolServiceMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
