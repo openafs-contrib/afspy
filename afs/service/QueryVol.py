6299af96b53c7949d566d1a97e3c7abb1d91ce02
hanke <hanke@b2240afc-07ac-11e0-b136-6b3c288fb16b>
2011-09-30 13:14:14 +0000

    afspy-service: - more refactoring
    git-svn-id: http://pfanne.rzg.mpg.de/svn/ro/hanke/openafs-plumbing/afspy@87 b2240afc-07ac-11e0-b136-6b3c288fb16b

--- afs/service/QueryVol.py
+++ afs/service/QueryVol.py
@@ -21,3 +21,55 @@ class QueryVol(object):
     """
     def getVolList(self,volQuery,**kwargs):
         pass
+    
+    ###############################################
+    # Volume Management Section
+    ###############################################
+   
+    def loadVol(self, servername=None, **kwargs):
+        
+        # This function is good only with db
+        if not self._CFG.DB_CACHE:
+             return
+         
+        cellname = self._TOKEN._CELL_NAME
+        
+        if kwargs.get("cellname"):
+            cellname = kwargs.get("cellname")
+        
+        volIdList = []
+        servers = []
+        
+        #Create a Server List
+        if servername:
+            server = self._srvDAO.getServer( servername, cellname)  
+            servers.append(server) 
+        else:
+            servers = self._srvDAO.getServerList(cellname)
+        
+        #Get DB Session
+        session = self.DbSession()
+           
+            
+        for server in servers:
+            #List of partitions in the specific server
+            parts = self._srvDAO.getPartList(server.name,cellname)
+            
+            for part in parts:
+                volDbList = session.query(Volume).filter(Volume.serv == server.name).filter(Volume.part == part.name)
+                #List of volume in the specific server and specific partition
+                volSvrList = self._srvDAO.getVolIdList( part.name, server.name, cellname)
+                for vol in volDbList: 
+                    self._volDAO.getVolume(vol.vid, vol, cellname)
+                    if volSvrList.has_key(vol.vid):  
+                        volSvrList.pop(vol.vid)
+                    else:
+                        session.delete(vol)
+                    
+                session.flush()
+                for vid in volSvrList:
+                     vol = Volume()
+                     self._volDAO.getVolume(vid, vol, cellname, self._TOKEN)
+                session.flush()
+            session.commit()      
+ 
--- afs/service/VolService.py
+++ afs/service/VolService.py
@@ -31,8 +31,7 @@ class VolService (object):
         if self._CFG.DB_CACHE:
             from afs.orm.DbMapper import DbMapper
             self.DbSession     = DbMapper(['Volume'])
-         
-         
+
     ###############################################
     # Volume Section
     ###############################################    
@@ -79,106 +78,11 @@ class VolService (object):
         pass
     
     
-    """
-    Retrieve Volume List
-    """
-    def getVolList(self,volQuery,**kwargs):
-        pass
-    
-    """
-    Retrieve Volume ID List
-    """
-    def getVolIdList(self,servername, partname=None,**kwargs):
-        cellname = self._TOKEN._CELL_NAME
-        vols = []
-        if kwargs.get("cellname"):
-            cellname = kwargs.get("cellname")
-            
-        if partname:    
-            vols = self._svrDAO.getVolIdList(partname, servername,cellname)
-        else:
-            parts = self._svrDAO.getPartList(servername,cellname)
-            for part in parts:
-                vols.extend(self._svrDAO.getVolIdList(part.name, servername,cellname))
-    
-        return vols
-    
-    ###############################################
-    # File Server Section
-    ###############################################
-    
-    """
-    Retrieve Server List
-    """
-    def getFileServerList(self,srvQuery,**kwargs):
-        pass
-    
-    
-    """
-    Retrieve Server 
-    """
-    def getFileServer(self,servername,**kwargs):
-        cellname = self._TOKEN._CELL_NAME
-     
-        if kwargs.get("cellname"):
-            cellname = kwargs.get("cellname")
-            
-        server = self._svrDAO.getServer(self,servername,cellname)
-        
-        parts = self._svrDAO.getPartList(server.name,cellname)
-        server.parts = parts
 
-        return server
+    
+
+    
+    
     
  
-    ###############################################
-    # Volume Management Section
-    ###############################################
-   
-    def loadVol(self, servername=None, **kwargs):
-        
-        # This function is good only with db
-        if not self._CFG.DB_CACHE:
-             return
-         
-        cellname = self._TOKEN._CELL_NAME
-        
-        if kwargs.get("cellname"):
-            cellname = kwargs.get("cellname")
-        
-        volIdList = []
-        servers = []
-        
-        #Create a Server List
-        if servername:
-            server = self._srvDAO.getServer( servername, cellname)  
-            servers.append(server) 
-        else:
-            servers = self._srvDAO.getServerList(cellname)
-        
-        #Get DB Session
-        session = self.DbSession()
-           
-            
-        for server in servers:
-            #List of partitions in the specific server
-            parts = self._srvDAO.getPartList(server.name,cellname)
-            
-            for part in parts:
-                volDbList = session.query(Volume).filter(Volume.serv == server.name).filter(Volume.part == part.name)
-                #List of volume in the specific server and specific partition
-                volSvrList = self._srvDAO.getVolIdList( part.name, server.name, cellname)
-                for vol in volDbList: 
-                    self._volDAO.getVolume(vol.vid, vol, cellname)
-                    if volSvrList.has_key(vol.vid):  
-                        volSvrList.pop(vol.vid)
-                    else:
-                        session.delete(vol)
-                    
-                session.flush()
-                for vid in volSvrList:
-                     vol = Volume()
-                     self._volDAO.getVolume(vid, vol, cellname, self._TOKEN)
-                session.flush()
-            session.commit()      
- 
+    
