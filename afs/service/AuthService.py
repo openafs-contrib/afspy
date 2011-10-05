9c05d6d701cd9a8a85bab5407de1083a72d3ad94
hanke <hanke@b2240afc-07ac-11e0-b136-6b3c288fb16b>
2011-10-05 15:27:40 +0000

    afspy:   put some meat into AuthService
    git-svn-id: http://pfanne.rzg.mpg.de/svn/ro/hanke/openafs-plumbing/afspy@119 b2240afc-07ac-11e0-b136-6b3c288fb16b

--- afs/service/AuthService.py
+++ afs/service/AuthService.py
@@ -1,6 +1,5 @@
-
-
 import afs.model.Token
+from afs.dao import PAGDAO
 
 
 class TokenService():
@@ -10,10 +9,25 @@ class TokenService():
     ...
     """
     
-    def __init__(self,config=None):
-        pass
+    def __init__(self,conf=None):
+        """
+        """
+        if conf:
+            self._CFG = conf
+        else:
+            self._CFG = afs.defaultConfig
+
+    def get_artificial_Token(self,AFSID,AFSCell):
+        token = afs.model.Token.Token(AFSID, AFSCell)
+        return token
     
-    
-    def getToken(self,username,password,cellname):
-        token = Token(username,password,cellname)
+    def getToken(self):
+        """
+        XXX : Move to Authservice
+        """
+        if self._CFG.CRED_TYPE == "ShellToken" :
+            pagdao = PAGDAO.PAGDAO()
+            AFSID, Cellname = pagdao.getTokeninPAG(cellname=self._CFG.AFSCell)
+            Cellname=Cellname.lower()
+            token=afs.model.Token.Token(AFSID, Cellname)
         return token
