8577b920e6582e1383026a1f8d5f28d0c9d00a88
hanke <hanke@b2240afc-07ac-11e0-b136-6b3c288fb16b>
2011-10-05 16:19:03 +0000

    afspy: update Token Model Object : Attributes are now Cellname and AFSID (not username!)
    git-svn-id: http://pfanne.rzg.mpg.de/svn/ro/hanke/openafs-plumbing/afspy@120 b2240afc-07ac-11e0-b136-6b3c288fb16b

--- afs/model/Token.py
+++ afs/model/Token.py
@@ -1,18 +1,15 @@
-
-
-
 class Token(object):
     """
-    Model object of a Token... to be implemented
+    Model object of a Token... t
     """
     _CELL_NAME = ""
+    _AFS_ID = -1
 
 
-    def __init__(self,username,password,cellname):
+    def __init__(self,afsid,cellname):
         """
         initialize an empty object
         """
         #Check the cell
         self._CELL_NAME = cellname
-        
-        #
+        self._AFS_ID = afsid
