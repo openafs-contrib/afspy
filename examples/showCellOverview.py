fa9e4494b4495d413a47cb6974ecf2593b3d79d2
Christof Hanke <christof.hanke@rzg.mpg.de>
2012-02-01 15:39:32 +0100

    examples: showCellOverview
    - also printout basic partition information
    - add debugging code

--- examples/showCellOverview.py
+++ examples/showCellOverview.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, string
+import sys, os, string, logging
 sys.path.append("..")
 from afs.util.AfsConfig import AfsConfig, setupDefaultConfig
 from afs.util.options import define, options
@@ -15,14 +15,18 @@ afs.defaultConfig.CELL_NAME="ipp-garching.mpg.de"
 # init services
 CS = CellService()
 
+# uncomment if debug wanted
+#CS.Logger.setLevel(logging.DEBUG)
+
 # get a light-weight list of all fileservers :
 
-FileServerList=CS.getFsList()
+FileServerList=CS.getFsList(includeParts=True)
 print "FileServers"
 print "========="
 for srv in FileServerList :
     print "%s: %s" %( string.join(srv.servernames, ","), string.join(srv.ipaddrs) )
-
+    for p in srv.parts :
+        print "\t%s, total: %u free: %u " % (p.name, p.size, p.free)
 # get a light-weight list of all dbservers
 
 print "DBServer"
