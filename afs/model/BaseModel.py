e77a232722ef81dacbd09fa17efdc695fe4478ca
Christof Hanke <christof.hanke@rzg.mpg.de>
2012-04-03 03:19:48 +0200

    Basemodel: use only __repr__
    which only prints out class-specific attributes

--- afs/model/BaseModel.py
+++ afs/model/BaseModel.py
@@ -6,20 +6,6 @@ class BaseModel(object):
     """
     The mother of all models.
     """
-    def __str__(self):
-        """
-        Get a string representation of the model object
-        """
-        res = ""
-        for attr, value in self.__dict__.iteritems():
-            if type(value) is IntType or type(value) is StringType or type(value) is LongType or type(value) is UnicodeType:
-                res += " %s=%s \n" % ( attr, value)
-            elif type(value) is ListType :
-                res += "%s= [\n" % attr
-                for i in range(len(value)) :
-                    res += "\t %s \n" %(value[i])
-                res += "]\n"
-        return res
     
     def setByDict(self,objByDict):      
         """
@@ -29,20 +15,13 @@ class BaseModel(object):
         for key, value in objByDict.iteritems():
             if not hasattr(self,key) :
                 raise AfsError("Cannot create new attribute '%s' to object '%s'" %(key, self.__class__.__name__))
+            # do not alter DB internal id and creation date
             if key != "id" and key != "cdate":
                 setattr(self,key,value)
           
     def copyObj(self, obj):
-        self.setByDict(obj.getDict())        
-    #FIXME
-    #def update(self,obj):
-    #    for attr, value in obj.__dict__.iteritems():
-    #        
-    #        if (type(attr) is IntType or type(attr) is StringType or type(attr) is LongType or type(attr) is UnicodeType\
-    #            or isinstance(attr, datetime.datetime) ):
-    #            print attr
-    #            if attr != "id":
-    #                setattr(self,attr,value)
+        self.setByDict(obj.getDict())
+        return
     
     def getJson(self):
         """
@@ -54,7 +33,6 @@ class BaseModel(object):
                 res[attr] = value.isoformat('-')
              elif isinstance(value, str) or isinstance(value, long) or isinstance(value,unicode) or isinstance(value, int):
                   res[attr] = value
-             
         return res
     
     def getDict(self):
@@ -65,7 +43,18 @@ class BaseModel(object):
         for attr, value in self.__dict__.iteritems():
             if isinstance(value, str) or isinstance(value, long) or isinstance(value,unicode) or isinstance(value, int) or isinstance(value, datetime.datetime):
                   res[attr] = value
-             
         return res
-        
     
+    def __repr__(self):
+        """
+        Get a string representation of the model object.
+        no python built-ins nor BaseModel stuff
+        """
+        BaseModelAttributes=dir(BaseModel())
+        repr="<%s(" % self.__class__.__name__
+        ModelAttributes=dir(self)
+        for a in ModelAttributes :
+            if a in BaseModelAttributes : continue
+            repr += "%s=%s, " % (a, eval("self.%s" % a))
+        repr += ")>"
+        return repr
