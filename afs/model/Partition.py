0cd528db7fc119d386e8a32b557991324ccccd3e
root <root@afs10.rzg.mpg.de>
2012-08-31 11:00:46 +0200

    db-maping: add new attributes to Partition
    allocated : how much quota are given on this partition
    allocated_stale : same as allocated, but in volumes
       which haven't been accessed lately.
    unLimitedVolumes : number of volumes with no quota

--- afs/model/Partition.py
+++ afs/model/Partition.py
@@ -7,30 +7,37 @@ class Partition(BaseModel):
     Model object of  a Partition
     """
        
-    def __init__(self, id='', serv_uuid='', name='', free=-1, size=-1, used=-1, usedPerc=-1):
+    def __init__(self, id='', serv_uuid='', name='', free=-1, size=-1, allocated=-1, allocated_stale = -1,used=-1, usedPerc=-1,unLimVol=-1):
         """
         initialize an empty object
         """
         ## DB - ID
         self.id = None
         ## UUID of fileserver
-        self.serv_uuid   = serv_uuid
+        self.serv_uuid = serv_uuid
         ## canonicalized partition name e.g "ad" for "/vicepad"
-        self.name   = name
+        self.name = name
         ## device-file
         self.device = ''
         ## CSV-list of projects-id 
         ## json-encodedlist of projectIDs having volumes on that partition
         self.projectIDs_js       = '[]'
         self.projectIDs=None
-        ## free size in bytes
+        ## free size in Kbytes
         self.free   = free
-        ## total size in bytes
+        ## total size in Kbytes
         self.size   = size
-        ## used size in bytes
-        self.used   = used
-        ## used perc in bytes
-        self.usedPerc   = usedPerc
+        ## allocated (by quota) size in Kbytes, useless if we have volumes with unlimited quota
+        self.allocated = allocated
+        ## stale_allocated, same as allocated, 
+        ## but for volumes which had not been accessed in $StaleTime days
+        self.allocated_stale = allocated_stale
+        ## used size in Kbytes
+        self.used = used
+        ## used size in percentage 
+        self.usedPerc = usedPerc
+        ## number of volumes with unlimited quota
+        self.unLimitedVolumes = unLimVol
         ## ?
         self.status = ''
         ## ?
