e8840d4812db6ef05f75ac9b4dff07f2d3405512
Christof Hanke <christof.hanke@rzg.mpg.de>
2013-01-20 13:55:13 +0100

    DBManager: make sure Objects are expunged from sqlalchemy
    session

--- afs/util/DBManager.py
+++ afs/util/DBManager.py
@@ -2,6 +2,7 @@ import string,json,logging
 from afs.exceptions.AfsError import AfsError
 from sqlalchemy import or_,func
 import sqlalchemy.orm.session
+from sqlalchemy.orm import object_session 
 import afs
 from types import ListType,DictType,StringType,IntType
 
@@ -46,7 +47,7 @@ class DBManager :
         return res
         
 
-    def getFromCache(self, Class, mustBeunique=True,**where) :
+    def getFromCache(self, Class, mustBeUnique=True, **where) :
         """
         get an object from the cache.
         unique is used the same way as in setIntoCache
@@ -54,11 +55,14 @@ class DBManager :
         query=self.DbSession.query(Class).filter_by(**where)
         cachedObjList = query.all()
         emptyObj=Class()
-        for c in cachedObjList :
-            c.updateAppRepr()
+        self.Logger.debug("getFromCache: got fromDB: %s" % (cachedObjList))
+        for r in cachedObjList :
+            r.updateAppRepr()
             # the ignAttrList is not stored in DB, thus add it here explictly.
-            c.ignAttrList=emptyObj.ignAttrList
-        if mustBeunique :
+            r.ignAttrList=emptyObj.ignAttrList
+            # expung Obj from Session, we don't want to use it outside
+            self.DbSession.expunge(r)
+        if mustBeUnique :
             if len(cachedObjList) > 1 :
                 raise AfsError("Constraints %s return no unique Object from DB" % where)
             elif len(cachedObjList) == 1 :
@@ -68,7 +72,7 @@ class DBManager :
         else :
             return cachedObjList
 
-    def getFromCacheByListElement(self,Class,Attr,Elem) :
+    def getFromCacheByListElement(self, Class, Attr, Elem, mustBeUnique=True) :
         """
         use hand-craftet query to search for a single element
         in a json-encoded list.
@@ -79,47 +83,116 @@ class DBManager :
             RegEx="\[({0}|.*, {0}|{0},.*|.*, {0},.*)\]".format(Elem)
         
         emptyObj=Class()
-        self.Logger.debug("getFromCacheByListElement, regex is : %s" % (RegEx))
-        resObjs=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
-        if len(resObjs) == 0 :
+        cachedObjList=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
+        if len(cachedObjList) == 0 :
+            self.Logger.debug("getFromCacheByListElement: returning None")
             return None
-        for r in resObjs :
+        for r in cachedObjList :
             r.updateAppRepr()
             # the ignAttrList is not stored in DB, thus add it here explictly.
             r.ignAttrList=emptyObj.ignAttrList
-        return resObjs
+            # expung Obj from Session, we don't want to use it outside
+            self.DbSession.expunge(r)
+        self.Logger.debug("getFromCacheByListElement: returning: %s" % (cachedObjList))
+        if mustBeUnique :
+            if len(cachedObjList) > 1 :
+                raise AfsError("getFromCacheByListElement: constraint Elem=%s returns no unique Object from DB" % (Elem))
+            elif len(cachedObjList) == 1 :
+                return cachedObjList[0]
+            else :
+                return None
+        else :
+            return cachedObjList
+        return cachedObjList
 
-    def getFromCacheByDictKey(self,Class,Attr,Elem) :
+    def getFromCacheByDictKey(self, Class, Attr, Key, mustBeUnique=True) :
         """
         use hand-craftet query to search for a single key
         in a json-encoded dict.
         """
-        RegEx="\{.*\"{0}\"\s+:.*\}".format(Elem)
+        RegEx='{{.*"{0}":.*}}'.format(Key)
+        self.Logger.debug("getFromCacheByDictKey: regex=%s" % (RegEx))
         emptyObj=Class()
-        resObjs=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
-        if len(resObjs) == 0 :
+        cachedObjList=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
+        if len(cachedObjList) == 0 :
+            self.Logger.debug("getFromCacheByDictKey: returning None")
             return None
-        for r in resObjs :
+        for r in cachedObjList :
             r.updateAppRepr()
             # the ignAttrList is not stored in DB, thus add it here explictly.
             r.ignAttrList=emptyObj.ignAttrList
-        return resObjs
+            # expung Obj from Session, we don't want to use it outside
+            self.DbSession.expunge(r)
+        self.Logger.debug("getFromCacheByDictKey: returning: %s" % (cachedObjList))
+        if mustBeUnique :
+            if len(cachedObjList) > 1 :
+                raise AfsError("getFromCacheByDictKey: constraint Key=%s returns no unique Object from DB" % (Key))
+            elif len(cachedObjList) == 1 :
+                return cachedObjList[0]
+            else :
+                return None
+        else :
+            return cachedObjList
+        return cachedObjList
 
-    def getFromCacheByDictValue(self,Class,Attr,Elem) :
+    def getFromCacheByDictValue(self, Class, Attr, Value, mustBeUnique=True) :
         """
         use hand-craftet query to search for a single Value
         in a json-encoded dict.
         """
-        RegEx="\{.*:\s+\"{0}\".*\}".format(Elem)
+        RegEx='{{.*: "{0}".*}}'.format(Value)
+        self.Logger.debug("getFromCacheByDictValue: regex=%s" % (RegEx))
         emptyObj=Class()
-        resObjs=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
-        for r in resObjs :
+        cachedObjList=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
+        if len(cachedObjList) == 0 :
+            self.Logger.debug("getFromCacheByDictValue : returning None")
+            return None
+        for r in cachedObjList :
             r.updateAppRepr()
             # the ignAttrList is not stored in DB, thus add it here explictly.
             r.ignAttrList=emptyObj.ignAttrList
-        return resObjs
+            # expung Obj from Session, we don't want to use it outside
+            self.DbSession.expunge(r)
+        self.Logger.debug("getFromCacheByDictValue : returning: %s" % (cachedObjList))
+        if mustBeUnique :
+            if len(cachedObjList) > 1 :
+                raise AfsError("getFromCacheByDictKeyValue: constraint Value=%s returns no unique Object from DB" % (Value))
+            elif len(cachedObjList) == 1 :
+                return cachedObjList[0]
+            else :
+                return None
+        else :
+            return cachedObjList
+        return cachedObjList
 
-    def setIntoCache(self,Class,Obj,uniqType="",**unique) :
+    def getFromCacheByDictKeyValuePair(self, Class, Attr, Key, Value, mustBeUnique=True) :
+        """
+        use hand-craftet query to search for a single Value
+        in a json-encoded dict.
+        """
+        RegEx='{{.*"{0}": "{1}".*}}'.format(Key,Value)
+        self.Logger.debug("getFromCacheByDictKeyValuePair: regex=%s" % (RegEx))
+        emptyObj=Class()
+        cachedObjList=self.DbSession.query(Class).filter(Attr.op('regexp')(RegEx)).all()
+        if len(cachedObjList) == 0 :
+            self.Logger.debug("getFromCacheByDictKeyValuePair: returning None")
+            return None
+        for r in cachedObjList :
+            r.updateAppRepr()
+            # the ignAttrList is not stored in DB, thus add it here explictly.
+            r.ignAttrList=emptyObj.ignAttrList
+        self.Logger.debug("getFromCacheByDictKeyValuePair: returning: %s" % (cachedObjList))
+        if mustBeUnique :
+            if len(cachedObjList) > 1 :
+                raise AfsError("getFromCacheByDictKeyValuePair: constraint Key=%s, Value=%s return no unique Object from DB" % (Key,Value))
+            elif len(cachedObjList) == 1 :
+                return cachedObjList[0]
+            else :
+                return None
+        else :
+            return cachedObjList
+
+    def setIntoCache(self,Class,Obj,**unique) :
         """ 
         Store an object into the cache.
         unique is a list of (Attribute-Name = value)-pairs which identifies 
@@ -127,34 +200,42 @@ class DBManager :
         A complex Attribute which is json encoded has to be dealt with differently.
         See setIntoCacheByDictKey,setIntoCacheByDictValue and setIntoCacheByListElement for this.
         """ 
-        self.Logger.debug("setInto Cache: called with class=%s, Obj=%s, **unique=%s " % (Class,Obj,unique))
+        self.Logger.debug("setIntoCache: called with class=%s, Obj=%s, **unique=%s " % (Class, Obj, unique))
+        # we have to detach the Obj from the sqlalchemy session, otherwise
+        # we mix Obj and mappedObj
+        if object_session(Obj) != None : 
+            self.DbSession.expunge(Obj)
+        self.Logger.debug("setIntoCache: Obj=%s" % Obj)
         # get a mapped object
         mappedObj = self.getFromCache(Class,**unique)
         if mappedObj == None :
             mappedObj=Class()
-        self.Logger.debug("got %s" % mappedObj)
         return self.do_setIntoCache(Obj,mappedObj)
 
-    def setFromCacheByDictValue(self,Class,Obj,Attr,Elem) :
+    def setIntoCacheByDictValue(self,Class,Obj,Attr,Elem) :
+        if object_session(Obj) != None : 
+            self.DbSession.expunge(Obj)
         # get a mapped object
-        mappedObj = getFromCacheByDictValue(Class,Attr,Elem)
-        self.Logger.debug("got %s" % mappedObj)
+        mappedObj = self.getFromCacheByDictValue(Class,Attr,Elem)
+        if mappedObj == None :
+            mappedObj=Class()
         return self.do_setIntoCache(Obj,mappedObj)
 
     def setIntoCacheByListElement(self,Class, Obj, Attr, Elem) :
+        if object_session(Obj) != None : 
+            self.DbSession.expunge(Obj)
         # get a mapped object
-        mappedObj = getFromCacheByDictValue(Class,Attr,Elem)
-        self.Logger.debug("got %s" % mappedObj)
+        mappedObj = self.getFromCacheByDictValue(Class,Attr,Elem)
         return self.do_setIntoCache(Obj,mappedObj)
 
     def do_setIntoCache(self,Obj,mappedObj) : 
         # copy over used Attributes
-        self.Logger.debug("got obj=%s" % Obj)
-        mappedObj.copyObj(Obj) # copyObj is defined in BaseClass 
-        self.Logger.debug("copied to mapped-Obj=%s" % mappedObj)
+        self.Logger.debug("do_setIntoCache: got obj=%s" % Obj)
+        mappedObj.copyObj(Obj) # copyObj is defined in BaseModel 
+        self.Logger.debug("do_setIntoCache: copied to mapped-Obj=%s" % mappedObj)
         # update database (json) representations
         mappedObj.updateDBRepr()
-        self.Logger.debug("got %s" % mappedObj)
+        self.Logger.debug("do_setIntoCache: got mappedobj=%s" % mappedObj)
         # push it to the DB.
         self.DbSession.merge(mappedObj)  
         self.DbSession.commit()  
