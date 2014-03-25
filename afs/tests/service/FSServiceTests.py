64e478412ddd657eef0bdd9bf690d3fe803ce8d7
Christof Hanke <christof.hanke@rzg.mpg.de>
2014-03-25 09:33:45 +0100

    ASYNC: first implementation of an async service
    together with it's unit-test.
    not complete yet

--- afs/tests/service/FSServiceTests.py
+++ afs/tests/service/FSServiceTests.py
@@ -10,6 +10,45 @@ import afs
 from afs.service.FSService import FSService
 
 
+
+class TestFSServiceSetMethods_async(unittest.TestCase):
+    """             
+    Tests FSService Methods
+    """         
+                    
+    @classmethod    
+    def setUpClass(self):
+        """         
+        setup FSService
+        """     
+        self.FSService = FSService()
+        self.test_config = ConfigParser()
+        self.test_config.read(afs.CONFIG.setup)
+        self.FsName = self.test_config.get("FSService", "FS")
+        self.FsUUID = self.test_config.get("FSService", "FSUUID")
+        self.FsPartitions = self.test_config.get("FSService", "Partitions").split(",")
+        self.FsPartitions.sort()
+        self.fileserver=self.FSService.get_fileserver(self.FsName, async=True, cached=False)
+        
+    def test_partition_list(self) :
+        parts = []
+        for p in self.fileserver.parts :
+            parts.append(p.name)
+        parts.sort()
+        self.assertEqual(self.FsPartitions, parts)
+        return
+
+    def test_uuid(self) :
+        self.assertEqual(self.FsUUID, self.fileserver.uuid)
+        return
+        
+    def test_get_volumes(self) :
+        task_ident = self.FSService.get_volumes(self.fileserver, async=True, cached=False)
+        self.FSService.wait_for_task(task_ident)
+        volumes = self.FSService.get_task_result(task_ident)    
+        sys.stderr.write("num_vols=%s\n" % len(volumes,))
+        return
+
 class TestFSServiceSetMethods(unittest.TestCase):
     """
     Tests FSService Methods
@@ -42,7 +81,7 @@ class TestFSServiceSetMethods(unittest.TestCase):
         return
 
     def test_get_volumes(self) :
-        volumes = self.FSService.get_volumes(self.fileserver, async=False, cached=False) 
+        volumes = self.FSService.get_volumes(self.fileserver, part="a", async=False, cached=False) 
         sys.stderr.write("num_vols=%s\n" % len(volumes,))
         return
 
@@ -64,7 +103,7 @@ class TestFSServiceCachedMethods(unittest.TestCase):
         self.FsUUID = self.test_config.get("FSService", "FSUUID")
         self.FsPartitions = self.test_config.get("FSService", "Partitions").split(",")
         self.FsPartitions.sort()
-        self.fileserver=self.FSService.get_fileserver(self.FsName, cached=True)
+        self.fileserver=self.FSService.get_fileserver(self.FsName, async=False, cached=True)
 
     @classmethod
     def tearDownClass(self) :
@@ -72,7 +111,7 @@ class TestFSServiceCachedMethods(unittest.TestCase):
         remove history from DB
         """
         sys.stderr.write("removing historic classes")
-        self.FSService.DBManager.vaccuum_cache()
+        #self.FSService.DBManager.vaccuum_cache()
         return
 
     def test_partitionlist(self) :
@@ -88,19 +127,23 @@ class TestFSServiceCachedMethods(unittest.TestCase):
         return
 
     def test_get_volumes(self) :
-        volumes = self.FSService.get_volumes(self.fileserver, cached=True) 
-        sys.stderr.write("num_vols=%s\n" % (volumes,))
+        volumes = self.FSService.get_volumes(self.fileserver, part="a", async=False, cached=True) 
+        sys.stderr.write("num_vols=%s\n" % len(volumes,))
         return
 
 
 if __name__ == '__main__' :
     parse_commandline()
 
+    sys.stderr.write("Testing async methods to fill DB_CACHE\n")
+    sys.stderr.write("==============================\n")
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFSServiceSetMethods_async)
+    unittest.TextTestRunner(verbosity=2).run(suite)
     sys.stderr.write("Testing live methods to fill DB_CACHE\n")
     sys.stderr.write("==============================\n")
     suite = unittest.TestLoader().loadTestsFromTestCase(TestFSServiceSetMethods)
     unittest.TextTestRunner(verbosity=2).run(suite)
-    sys.stderr.write("Testing  methods accessing DB_CACHE\n")
+    sys.stderr.write("Testing sync methods accessing DB_CACHE\n")
     sys.stderr.write("================================\n")
     if afs.CONFIG.DB_CACHE :
         suite = unittest.TestLoader().loadTestsFromTestCase(TestFSServiceCachedMethods)
