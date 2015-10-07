ea375b764f438701ab936d62aa2774d20b83f690
Christof Hanke <christof.hanke@rzg.mpg.de>
2015-10-07 08:37:48 +0200

    deploy.sh:  force pythonversion 2.7

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rm -rf  /usr/local/lib/python2.7/site-packages/afs/; rm -rf build/ ; python ./setup.py install
+rm -rf  /usr/local/lib/python2.7/site-packages/afs/; rm -rf build/ ; python2.7 ./setup.py install
